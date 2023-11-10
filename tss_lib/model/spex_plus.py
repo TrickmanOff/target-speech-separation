a5cb083898983970ffc688efff5252acbcb16a16
TrickmanOff <egorkadv2002@gmail.com>
2023-11-10 14:43:02 +0300

    comments

--- tss_lib/model/spex_plus.py
+++ tss_lib/model/spex_plus.py
@@ -323,9 +323,9 @@ class SpexPlus(BaseModel):
             ref:  (batch_dim, 1, T_ref)
 
         output:
-            si:   (batch_dim, 1, T) - predicted target audio from mix
+            si:   (batch_dim, 1, T) - predicted targets audio from mix
                 for i = 1, 2, 3
-            speakers_logits: (batch_dim, num_classes) - predicted logits of target speaker
+            speakers_logits: (batch_dim, num_classes) - predicted logits of targets speaker
         """
 
         encoded_mix = self.speech_encoder(mix)
